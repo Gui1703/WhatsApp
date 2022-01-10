06f33f3cf59f9c6d9b445bd8ff63b8c20f50f2c2
Guilherme Raposo <gui.raposo36@gmail.com>
2022-01-10 15:11:55 -0300

    Update buttons

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="my-2 p-2 text-red-500">
-    <div class="flex flex-row w-96 space-x-7 items-center border-gray rounded-md bg-gray-900 p-2 mb-3">
+    <div class="flex flex-row w-96 space-x-7 items-center border-gray rounded-md bg-gray-900 px-3 p-2 mb-3">
       <i class="fas fa-ban"></i>
       <a>Bloquear</a>
     </div>
-    <div class="flex flex-row w-96 space-x-7 items-center border-gray rounded-md bg-gray-900 p-2">
+    <div class="flex flex-row w-96 space-x-7 items-center border-gray rounded-md bg-gray-900 px-3 p-2">
       <i class="fas fa-thumbs-down"></i>
       <a>Denunciar empresa</a>
     </div>
